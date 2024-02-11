88903fafb3fe7da735eb16b9a7b9f158d4485d05
ThzEdouard <edouard.flaquet@gmail.com>
2024-02-11 23:01:58 +0100

    module04

---
+++ module04/ex00/statistics.py
@@ -0,0 +1,42 @@
+def ft_statistics(*args: any, **kwargs: any) -> None:
+    numbers = [x for x in args if isinstance(x, (int, float))]
+    for kwarg in kwargs.values():
+        if "mean" in kwarg:
+            if not numbers:
+                print("ERROR")
+            else:
+                mean = sum(numbers) / len(numbers)
+                print(f"mean : {mean}")
+        elif "median" in kwarg:
+            if not numbers:
+                print("ERROR")
+            else:
+                sorted_numbers = sorted(numbers)
+                n = len(sorted_numbers)
+                median = (sorted_numbers[n // 2] if n % 2 != 0 else (sorted_numbers[n // 2 - 1] + sorted_numbers[n // 2]) / 2)
+                print(f"median : {median}")
+        elif "quartile" in kwarg:
+            if not numbers:
+                print("ERROR")
+            else:
+                sorted_numbers = sorted(numbers)
+                quartile_25_index = int(len(sorted_numbers) * 0.25)
+                quartile_75_index = int(len(sorted_numbers) * 0.75)
+                quartile_25 = float(sorted_numbers[quartile_25_index])
+                quartile_75 = float(sorted_numbers[quartile_75_index])
+                print(f"quartile : [{quartile_25}, {quartile_75}]")
+        elif "std" in kwarg:
+            if not numbers:
+                print("ERROR")
+            else:
+                mean = sum(numbers) / len(numbers)
+                variance = sum((x - mean) ** 2 for x in numbers) / len(numbers)
+                std_dev = variance ** 0.5
+                print(f"std : {std_dev}")
+        elif "var" in kwarg:
+            if not numbers:
+                print("ERROR")
+            else:
+                mean = sum(numbers) / len(numbers)
+                variance = sum((x - mean) ** 2 for x in numbers) / len(numbers)
+                print(f"var : {variance}")
---
+++ module04/ex02/callLimit.py
@@ -0,0 +1,16 @@
+from functools import wraps
+
+
+def callLimit(limit: int):
+    count = 0
+    def callLimiter(function):
+        @wraps(function)
+        def limit_function(*args: any, **kwds: any):
+            nonlocal count
+            if count < limit:
+                count += 1
+                return function(*args, **kwds)
+            else:
+                print(f"Function {function.__name__} called too many times.")
+        return limit_function
+    return callLimiter
